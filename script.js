0c473bd6272627f87bc693aa1048a11d99b78817
nicolecanhovattisoares-ui <nicole.canhovatti.soares@escola.pr.gov.br>
2026-06-16 12:28:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
+
+
+
+
+
+
+
+
+
 //O JavaScript é uma linguagem WEB
 
-//Variáveis - Valores armazenados na memória do computador 
+//Variaveis - Valores armazenados na memória do computador
 
-//Funções - Blocos de código 
+//Funções - Blocos de código
 
 function carregar(){
-    //alert("Bem vindo ao JavaScript")
-    var msg = window.document.getElementById
-    ('msg')
-    msg.innerHTML = `Desenvolvendo com DOM`
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //alert("Bem Vindo!")
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    
+    
+    
+    msg.innerHTML = `Agora são ${hora}:${minutos}`
+    
+    if(hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+        document.body.style.background = '#00b4d8'
+    }
+    else if(hora >= 12 && hora < 18){
+        img.src = 'tarde.png'
+  
+        document.body.style.background = ' #ff0000'
+    }
+    else{
+        img.src = 'noite.png'
+        document.body.style.background = 'linear-gradient(to right, #ff0000, #ff7f00, #ffff00, #00ff00, #0000ff, #8b00ff)'
+    }
+    
+    }
 
